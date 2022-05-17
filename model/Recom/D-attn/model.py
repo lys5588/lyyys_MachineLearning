d75aba565b6d6b20ac9562413681e89bbd7f2ba8
Yunshan Li <54197852+lys5588@users.noreply.github.com>
2022-05-17 13:43:56 +0800

    Update model.py

--- model/Recom/D-attn/model.py
+++ model/Recom/D-attn/model.py
@@ -4,8 +4,10 @@ from torch.autograd import Variable
 
 
 class LocalAttention(nn.Module):
-    def __init__(self, input_size, embed_size, win_size, out_channels):
+    def __init__(self, input_size, embed_size, win_size, out_channels,device,vocab_size=8000):
         super(LocalAttention, self).__init__()
+
+        self.device=device
 
         self.input_size = input_size
         self.embed_size = embed_size
@@ -13,11 +15,13 @@ class LocalAttention(nn.Module):
         self.win_size = win_size
         self.out_channels = out_channels
 
+        self.embedding=nn.Embedding(vocab_size+1,embed_size)
         self.attention_layer = nn.Sequential(
             nn.Conv2d(1, 1, kernel_size=(self.win_size, self.embed_size)),
             nn.Sigmoid()
         )
-
+        #input(100,10000,100)
+        #(1,200,(1,100))
         self.cnn = nn.Sequential(
             nn.Conv2d(1, self.out_channels, kernel_size=(1, self.embed_size)),
             nn.Tanh(),
@@ -25,11 +29,14 @@ class LocalAttention(nn.Module):
         )
 
     def forward(self, x):
+        #1. 计算s(t)
+        #100*10000->100*10000*100->100*10000*104
         #padding: input_size , (window_size-1)/2 , embed_size
-        padding = Variable(torch.zeros(x.size(0), int((self.win_size - 1) / 2), self.embed_size))
-        torch.zeros(x.size(0), int((self.win_size - 1) / 2), self.embed_size)
+        padding = Variable(torch.zeros(x.size(0), int((self.win_size - 1) / 2), self.embed_size)).to(self.device)
+        # torch.zeros(x.size(0), int((self.win_size - 1) / 2), self.embed_size)
         # padding = padding.cuda()
-        x_pad = torch.cat((padding, x, padding), 1)
+        embed=self.embedding(x.int())
+        x_pad = torch.cat((padding, embed, padding), 1)
 
 
         x_pad = x_pad.unsqueeze(1)
@@ -37,7 +44,7 @@ class LocalAttention(nn.Module):
 
         scores = scores.squeeze(1)
 
-        out = torch.mul(x, scores)
+        out = torch.mul(embed, scores)
 
         out = out.unsqueeze(1)
         out = self.cnn(out)
@@ -46,16 +53,21 @@ class LocalAttention(nn.Module):
 
 
 class GlobalAttention(nn.Module):
-    def __init__(self, input_size, embed_size, out_channels):
+    def __init__(self, input_size, embed_size, out_channels,device,vocab_size=8000):
         super(GlobalAttention, self).__init__()
+        self.device=device
 
         self.input_size = input_size
         self.embed_size = embed_size
         self.out_channels = out_channels
 
+        self.embedding = nn.Embedding(vocab_size + 1, embed_size)
+
         self.attention_layer = nn.Sequential(
             nn.Conv2d(1, 1, kernel_size=(self.input_size, self.embed_size)),
             nn.Sigmoid())
+
+        #input: 100,1,100,10000
 
         self.cnn_1 = nn.Sequential(
             nn.Conv2d(1, self.out_channels, kernel_size=(2, self.embed_size)),
@@ -73,9 +85,10 @@ class GlobalAttention(nn.Module):
             nn.MaxPool2d((self.input_size - 4 + 1, 1)))
 
     def forward(self, x):
-        x = x.unsqueeze(1)
-        score = self.attention_layer(x)
-        out = torch.mul(x, score)
+        embed = self.embedding(x.int())
+        embed = embed.unsqueeze(1)
+        score = self.attention_layer(embed)
+        out = torch.mul(embed, score)
         out_1 = self.cnn_1(out)
         out_2 = self.cnn_2(out)
         out_3 = self.cnn_3(out)
@@ -84,14 +97,17 @@ class GlobalAttention(nn.Module):
 
 class CNNDLGA(nn.Module):
 
-    def __init__(self, input_size, embed_size=100, win_size=5, channels_local=200, channels_global=100,
+    def __init__(self, input_size,device, embed_size=100, win_size=5, channels_local=200, channels_global=100,
                  fc_input_size=500, hidden_size=500, output_size=50):
         super(CNNDLGA, self).__init__()
-
-        self.localAttentionLayer_user = LocalAttention(input_size, embed_size, win_size, channels_local)
-        self.globalAttentionLayer_user = GlobalAttention(input_size, embed_size, channels_global)
-        self.localAttentionLayer_item = LocalAttention(input_size, embed_size, win_size, channels_local)
-        self.globalAttentionLayer_item = GlobalAttention(input_size, embed_size, channels_global)
+        self.device=device
+        self.localAttentionLayer_user = LocalAttention(input_size, embed_size, win_size, channels_local,device=device)
+        self.globalAttentionLayer_user = GlobalAttention(input_size, embed_size, channels_global,device=device)
+        self.localAttentionLayer_item = LocalAttention(input_size, embed_size, win_size, channels_local,device=device)
+        self.globalAttentionLayer_item = GlobalAttention(input_size, embed_size, channels_global,device=device)
+        #input:100*500
+        #1. 500,500 2.drop 3. relu 4. 500,50
+        #output:100*50
         self.fcLayer = nn.Sequential(
             nn.Linear(fc_input_size, hidden_size),
             nn.Dropout(0.5),
@@ -105,6 +121,7 @@ class CNNDLGA(nn.Module):
         global1_user, global2_user, global3_user = self.globalAttentionLayer_user(x_user)
         out_user = torch.cat((local_user, global1_user, global2_user, global3_user), 1)
         out_user = out_user.view(out_user.size(0), -1)
+        #out_user:100*500
         out_user = self.fcLayer(out_user)
 
         # item
